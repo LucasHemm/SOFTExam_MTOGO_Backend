62243a27eee22ce4af533227a75dc7ecb74ab4c2
Matias Rottbøll <mrottboell@gmail.com>
2024-11-20 12:43:29 +0100

    tests workflow

--- MTOGO/MTOGOTEST/obj/Debug/net8.0/MTOGOTEST.AssemblyInfo.cs
+++ MTOGO/MTOGOTEST/obj/Debug/net8.0/MTOGOTEST.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MTOGOTEST")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14626f6fa1cef1110393c716ae74b4bc1afc402d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92def69728be8472926af67cc08decf8547dfed3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MTOGOTEST")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MTOGOTEST")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
