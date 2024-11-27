8c7a3baacb90c935aee2133c153293ebacb1667e
Matias Rottbøll <mrottboell@gmail.com>
2024-11-27 15:46:26 +0100

    review

---
+++ MTOGO/MTOGO/DTOs/UpdateRatingDTO.cs
@@ -0,0 +1,20 @@
+﻿namespace MTOGO.DTOs;
+
+public class UpdateRatingDTO
+{
+    public int Id { get; set; } //primary key
+    public double Rating { get; set; }
+    public int NumberOfRatings { get; set; }
+
+
+    public UpdateRatingDTO()
+    {
+    }
+
+    public UpdateRatingDTO(int id, double rating, int numberOfRatings)
+    {
+        Id = id;
+        Rating = rating;
+        NumberOfRatings = numberOfRatings;
+    }
+}
